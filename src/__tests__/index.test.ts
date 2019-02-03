c1fc42bc35430912f4d9e7eaaa0f7dcaacd53c68
christiandrey <oluwaseun.adedire@gmail.com>
2019-02-03 23:10:34 +0100

    Added color interpolate functions

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,4 +1,4 @@
-import { linearInterpolate, lagrangeInterpolate } from "../index";
+import { linearInterpolate, lagrangeInterpolate, colorInterpolate } from "../index";
 
 describe("linearInterpolate", () => {
 	test("Should accept as input range, an array whose length is 2", () => {
@@ -27,9 +27,6 @@ describe("linearInterpolate", () => {
 	test("Should not allow two equal values in both input range", () => {
 		expect(() => linearInterpolate([1, 1], [4, 5], 3)).toThrowError("No two values in the input range can be equal.");
 	});
-	test("Should not allow two equal values in both output range", () => {
-		expect(() => linearInterpolate([1, 2], [4, 4], 3)).toThrowError("No two values in the output range can be equal.");
-	});
 });
 
 describe("lagrangeInterpolate", () => {
@@ -47,11 +44,36 @@ describe("lagrangeInterpolate", () => {
 			"Only numbers are allowed to be values of the input range, output range and the value to solve for."
 		);
 	});
-	test("Should not allow any two equal values in the input range and the output range", () => {
-		expect(() => lagrangeInterpolate([1, 1, 3], [4, 5, 6], 2)).toThrowError("No two values in the input range and output range can be equal.");
-		expect(() => lagrangeInterpolate([1, 2, 3], [4, 4, 6], 2)).toThrowError("No two values in the input range and output range can be equal.");
+	test("Should not allow any two equal values in the input range", () => {
+		expect(() => lagrangeInterpolate([1, 1, 3], [4, 5, 6], 2)).toThrowError("No two values in the input range can be equal.");
 	});
 	test("Should solve to ", () => {
 		expect(lagrangeInterpolate([-2, 1, 3, 7], [5, 7, 11, 34], 0)).toBeCloseTo(1087 / 180);
+	});
+});
+
+describe("colorInterpolate", () => {
+	test("Should allow only an equal size of input range and output range", () => {
+		expect(() => colorInterpolate([1, 3], ["#ff0000", "#ff00ff", "#ffff00"], 2)).toThrowError("The input range and output range must have equal sizes.");
+	});
+	test("Should accept only numbers as input range", () => {
+		expect(() => colorInterpolate([1, "2"], ["#ff0000", "#ff00ff"], 1.5)).toThrowError("The values in the input range can only be numbers.");
+	});
+	test("Should accept only strings as output range", () => {
+		expect(() => colorInterpolate([1, 2], [1, "#ff00ff"], 1.5)).toThrowError("The values in the output range can only be strings.");
+	});
+	test("Should accept only strings that are valid colors in the output range", () => {
+		expect(() => colorInterpolate([1, 2], ["#ff0000", "#ff00gh"], 1.5)).toThrowError("The values in the output range have to be valid hex or rgb colors.");
+		expect(() => colorInterpolate([1, 2], ["#ff0000", "#bag"], 1.5)).toThrowError("The values in the output range have to be valid hex or rgb colors.");
+		expect(() => colorInterpolate([1, 2], ["#ff0000", "invalid value"], 1.5)).toThrowError("The values in the output range have to be valid hex or rgb colors.");
+		expect(() => colorInterpolate([1, 2], ["#ff0000", "rgba(255,255,255,0.2)"], 1.5)).toThrowError("The values in the output range have to be valid hex or rgb colors.");
+		expect(() => colorInterpolate([1, 2], ["#ff0000", "rgb(255,255,500)"], 1.5)).toThrowError("The values in the output range have to be valid hex or rgb colors.");
+	});
+	test("Should not allow any two equal values in the input range and the output range", () => {
+		expect(() => colorInterpolate([1, 1, 3], ["#ff0000", "#ff00ff", "#ffff00"], 2)).toThrowError("No two values in the input range and output range can be equal.");
+		expect(() => colorInterpolate([1, 2, 3], ["#ff0000", "#ff0000", "#ffff00"], 2)).toThrowError("No two values in the input range and output range can be equal.");
+	});
+	test("Should solve to", () => {
+		expect(colorInterpolate([0, 1], ["#ff0000", "#ffffff"], 0.5)).toBe("#ff8080");
 	});
 });
--- src/index.ts
+++ src/index.ts
@@ -17,17 +17,14 @@ export function linearInterpolate(inputRange: Array<number>, outputRange: Array<
 	if (new Set(inputRange).size !== 2) {
 		throw new Error("No two values in the input range can be equal.");
 	}
-	if (new Set(outputRange).size !== 2) {
-		throw new Error("No two values in the output range can be equal.");
-	}
 
 	const increment = ((solveFor - inputRange[0]) * (outputRange[1] - outputRange[0])) / (inputRange[1] - inputRange[0]);
 	return outputRange[0] + increment;
 }
 
 export function lagrangeInterpolate(inputRange: Array<number>, outputRange: Array<number>, solveFor: number): number {
-	if (new Set(inputRange).size !== inputRange.length || new Set(outputRange).size !== outputRange.length) {
-		throw new Error("No two values in the input range and output range can be equal.");
+	if (new Set(inputRange).size !== inputRange.length) {
+		throw new Error("No two values in the input range can be equal.");
 	}
 	if (inputRange.length !== outputRange.length) {
 		throw new Error("The input range and output range must have equal sizes.");
@@ -39,11 +36,106 @@ export function lagrangeInterpolate(inputRange: Array<number>, outputRange: Arra
 	let value = 0;
 
 	for (let i = 0; i < inputRange.length; i++) {
-		const top = inputRange.filter((o, index) => index !== i).reduce((a, b) => a * (solveFor - b), 1);
-		const bottom = inputRange.filter((o, index) => index !== i).reduce((a, b) => a * (inputRange[i] - b), 1);
-		const increment = (top / bottom) * outputRange[i];
+		const numerator = inputRange.filter((o, index) => index !== i).reduce((a, b) => a * (solveFor - b), 1);
+		const denominator = inputRange.filter((o, index) => index !== i).reduce((a, b) => a * (inputRange[i] - b), 1);
+		const increment = (numerator / denominator) * outputRange[i];
 		value = value + increment;
 	}
 
 	return value;
+}
+
+export function colorInterpolate(inputRange: Array<number>, outputRange: Array<string>, solveFor): string {
+	if (inputRange.length !== outputRange.length) {
+		throw new Error("The input range and output range must have equal sizes.");
+	}
+	if (new Set(inputRange).size !== inputRange.length || new Set(outputRange).size !== outputRange.length) {
+		throw new Error("No two values in the input range and output range can be equal.");
+	}
+	if (!inputRange.every(o => typeof o === "number")) {
+		throw new Error("The values in the input range can only be numbers.");
+	}
+	if (!outputRange.every(o => typeof o === "string")) {
+		throw new Error("The values in the output range can only be strings.");
+	}
+	if (!outputRange.every(o => validateColor(o))) {
+		throw new Error("The values in the output range have to be valid hex or rgb colors.");
+	}
+
+	const normalizedValues = outputRange.map(o => normalizeColor(o));
+	const interpolatedValues = new Array<number>();
+	const useLinearInterpolation = inputRange.length === 2;
+
+	for (let i = 0; i < 3; i++) {
+		const outputRange = normalizedValues.map(o => o[i]);
+		useLinearInterpolation
+			? interpolatedValues.push(Math.round(linearInterpolate(inputRange, outputRange, solveFor)))
+			: interpolatedValues.push(Math.round(lagrangeInterpolate(inputRange, outputRange, solveFor)));
+	}
+
+	return "#" + interpolatedValues.map(o => convertDecimalToHexadecimal(o)).join("");
+}
+
+function validateColor(color: string): boolean {
+	const sanitizedColor = sanitizeColor(color);
+
+	if (validateHexadecimalColor(sanitizedColor)) {
+		return true;
+	}
+
+	if (validateRgbColor(sanitizedColor)) {
+		return true;
+	}
+
+	return false;
+}
+
+function normalizeColor(color: string): Array<number> {
+	let sanitizedColor = sanitizeColor(color);
+
+	if (validateHexadecimalColor(sanitizedColor)) {
+		sanitizedColor = sanitizedColor.replace("#", "");
+		if (sanitizedColor.length === 3) {
+			sanitizedColor = sanitizedColor
+				.split("")
+				.map(o => o + o)
+				.join("");
+		}
+
+		const normalizedColor = new Array<number>();
+		for (let i = 0; i < 6; i += 2) {
+			normalizedColor.push(convertHexadecimalToDecimal(`${sanitizedColor.charAt(i)}${sanitizedColor.charAt(i + 1)}`));
+		}
+
+		return normalizedColor;
+	}
+
+	return sanitizedColor
+		.match(/\((.*?)\)/)[1]
+		.split(",")
+		.map(o => parseInt(o));
+}
+
+function sanitizeColor(color: string): string {
+	return color.toLowerCase().replace(/\s\s+/, "");
+}
+
+function validateHexadecimalColor(color: string): boolean {
+	const hexTestRegex = /^#([a-f0-9]{3}|[a-f0-9]{6})$/;
+	return hexTestRegex.test(color);
+}
+
+function validateRgbColor(color: string): boolean {
+	const rgbTestRegex = /^rgb\(([0-2]?[0-9]{0,2},?){2}[0-2]?[0-9]{0,2}\)$/;
+	return rgbTestRegex.test(color);
+}
+
+function convertDecimalToHexadecimal(decimal: number): string {
+	const hexTable = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+	return `${hexTable[Math.floor(decimal / 16)]}${hexTable[decimal % 16]}`;
+}
+
+function convertHexadecimalToDecimal(hexadecimal: string): number {
+	const hexTable = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+	return hexTable.indexOf(hexadecimal.charAt(0)) * 16 + hexTable.indexOf(hexadecimal.charAt(1));
 }
